5fd453518f5e0123b168a98b1284ba8f8362e476
cris711 <47696796+cris711@users.noreply.github.com>
2019-02-16 21:01:24 -0600

    Add files via upload

---
+++ Homepage.js
@@ -0,0 +1,22 @@
+function weeklySpending(){
+    n0=10;
+    n1=12;
+    n2=13;
+    n3=5;
+    n4=0;
+    n5=20;
+    n6=78;
+    return n1,n2,n3,n4,n5,n6,n7;
+}
+/*function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+    ['', 'Hours per Day'],
+    ['Monday', n0]
+    ['Tuesday', n1],
+    ['Wednesday', n2],
+    ['Thursday', n3],
+    ['Friday', n4],
+    ['Saturday', n5]
+    ['Sunday', n6]
+    ]);
+}*/
